b8765b4de67326ed63a433be0713086a05844f94
Erez Oxman <erez.oxman@gmail.com>
2020-06-22 09:17:27 +0300

    Create member_groups_only_structs.h

---
+++ member_groups_only_structs.h
@@ -0,0 +1,22 @@
+/** @name API Types
+ *  Basic API Types
+ */
+///@{
+
+/** Comment struct b */
+struct struct_b {
+    int a;             /**< something */
+} struct_b;
+
+
+/** Comment struct a */
+struct struct_a {
+    float b;             /**< something else */
+} struct_a;
+
+///@}
+
+/** Comment struct c not in group */
+struct struct_c {
+    int c;             /**< something two*/
+} struct_c;
